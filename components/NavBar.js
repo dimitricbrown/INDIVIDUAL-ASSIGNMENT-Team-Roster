1cb89509e6edd0c9e87ace8476a22d82d9e3956f
dimitricbrown <dimitricbrown@outlook.com>
2023-04-25 18:40:50 -0500

    Navbar rendering and routing properly

--- components/NavBar.js
+++ components/NavBar.js
@@ -11,14 +11,17 @@ export default function NavBar() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+          <Navbar.Brand>Glee Club Roster</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
+        <Navbar.Collapse className="justify-content-end">
           <Nav className="me-auto">
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
+              <Nav.Link>New Directions</Nav.Link>
+            </Link>
+            <Link passHref href="/new">
+              <Nav.Link>New Gleek</Nav.Link>
             </Link>
             <Button variant="danger" onClick={signOut}>Sign Out</Button>
           </Nav>
---
+++ pages/new.js
@@ -0,0 +1,3 @@
+export default function AddMember() {
+  return <h1>Add a Member</h1>;
+}
